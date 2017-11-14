567e637e8a37a069d29918a4f5a8e945513333d2
Alex Guerra <alex@heyimalex.com>
2017-11-14 17:13:54 -0600

    Use prettier defaults
    Sorry single quotes

--- scripts/build-language-mapping.js
+++ scripts/build-language-mapping.js
@@ -3,12 +3,12 @@
 // block, to scope name, which is used to figure out the grammar to use for
 // highlighting.
 
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+const fs = require("fs");
+const path = require("path");
+const yaml = require("js-yaml");
 
-const languagesYmlPath = './linguist/lib/linguist/languages.yml';
-const languages = yaml.safeLoad(fs.readFileSync(languagesYmlPath, 'utf8'));
+const languagesYmlPath = "./linguist/lib/linguist/languages.yml";
+const languages = yaml.safeLoad(fs.readFileSync(languagesYmlPath, "utf8"));
 
 // Unfortunately, many of the entries in languages.yml don't have an
 // associated tm_scope. This is the case even for common languages like go and
@@ -16,7 +16,7 @@ const languages = yaml.safeLoad(fs.readFileSync(languagesYmlPath, 'utf8'));
 // _attempt_ to match them with the appropriate entry in languages.yml. Then
 // the language entry will be updated with the matched tm_scope.
 
-console.log('\nLoading grammars...');
+console.log("\nLoading grammars...");
 
 // Load all of the grammars and build a couple of indexes. To start, we'll
 // treat the indexes like multimaps, where the values are actually sets. Then
@@ -34,19 +34,19 @@ function multiMapAdd(map, key, value) {
 }
 
 const grammars = fs
-  .readdirSync('./grammars')
+  .readdirSync("./grammars")
   .map(grammarFileName => require(`../grammars/${grammarFileName}`));
 
 for (const grammar of grammars) {
-  if (grammar.hasOwnProperty('name')) {
+  if (grammar.hasOwnProperty("name")) {
     multiMapAdd(grammarsByName, slugify(grammar.name), grammar);
   }
 
-  if (grammar.hasOwnProperty('scopeName')) {
+  if (grammar.hasOwnProperty("scopeName")) {
     multiMapAdd(grammarsByScope, grammar.scopeName, grammar);
   }
 
-  if (grammar.hasOwnProperty('fileTypes')) {
+  if (grammar.hasOwnProperty("fileTypes")) {
     for (let fileType of grammar.fileTypes) {
       multiMapAdd(grammarsByExt, fileType.toLowerCase(), grammar);
     }
@@ -71,20 +71,19 @@ function multiMapDedup(map, label) {
   }
 }
 
-multiMapDedup(grammarsByName, 'name');
-multiMapDedup(grammarsByScope, 'scope');
-multiMapDedup(grammarsByExt, 'extension');
+multiMapDedup(grammarsByName, "name");
+multiMapDedup(grammarsByScope, "scope");
+multiMapDedup(grammarsByExt, "extension");
 
 // Iterate through the language entries and attempt to find the matching
 // tm_scope if it's not already present.
 
-console.log('\nAssociating scopes...');
+console.log("\nAssociating scopes...");
 
-langloop:
-for (let [name, language] of Object.entries(languages)) {
+langloop: for (let [name, language] of Object.entries(languages)) {
   // Skip languages that already have an associated tm_scope. This 'none'
   // convention is documented in languages.yml.
-  if (language.hasOwnProperty('tm_scope') && language.tm_scope !== 'none') {
+  if (language.hasOwnProperty("tm_scope") && language.tm_scope !== "none") {
     continue;
   }
 
@@ -96,7 +95,7 @@ for (let [name, language] of Object.entries(languages)) {
   }
 
   // Check for an extension match.
-  if (language.hasOwnProperty('extensions')) {
+  if (language.hasOwnProperty("extensions")) {
     for (let ext of language.extensions) {
       ext = ext.substring(1).toLowerCase(); // Remove leading period
       if (grammarsByExt.has(ext)) {
@@ -121,8 +120,8 @@ for (let [name, language] of Object.entries(languages)) {
 
 // Log all of the unused grammars for good measure.
 
-console.log('\nChecking for unused grammars...');
-console.log('Note that these may still be included from other grammars.');
+console.log("\nChecking for unused grammars...");
+console.log("Note that these may still be included from other grammars.");
 
 const unused = new Set([...grammarsByScope.keys()]);
 for (let [name, language] of Object.entries(languages)) {
@@ -134,7 +133,7 @@ for (const scopeName of unused.values()) {
   console.log(`  ${scopeName}`);
 }
 
-console.log('\nCreating indexes...');
+console.log("\nCreating indexes...");
 
 const index = new Map();
 const extensionIndex = new Map();
@@ -142,14 +141,14 @@ const extensionIndex = new Map();
 for (let [name, language] of Object.entries(languages)) {
   // We won't be able to highlight any languages that don't have an
   // associated grammar, so just skip these.
-  if (!language.hasOwnProperty('tm_scope')) {
+  if (!language.hasOwnProperty("tm_scope")) {
     continue;
   }
 
   const scopeName = language.tm_scope;
 
   function addToIndex(key) {
-    key = key.toLowerCase().replace(' ', '-');
+    key = key.toLowerCase().replace(" ", "-");
     // Check that there isn't already an entry under this key in the index.
     if (index.has(key)) {
       const otherScope = index.get(key);
@@ -166,7 +165,7 @@ for (let [name, language] of Object.entries(languages)) {
 
   function addToExtensions(extension) {
     // Remove leading dot
-    if (extension.startsWith('.')) {
+    if (extension.startsWith(".")) {
       extension = extension.substring(1);
     }
     extension = extension.toLowerCase();
@@ -184,21 +183,21 @@ for (let [name, language] of Object.entries(languages)) {
   }
 
   // Sanity check
-  if (typeof scopeName !== 'string') {
+  if (typeof scopeName !== "string") {
     throw new Error(`Invalid scope name for "${name}": ${scopeName}`);
   }
 
   // Add slugified versions of the name to the index.
   const lowerName = name.toLowerCase();
-  if (lowerName.includes(' ')) {
-    addToIndex(lowerName.replace(' ', '-'));
-    addToIndex(lowerName.replace(' ', '_'));
+  if (lowerName.includes(" ")) {
+    addToIndex(lowerName.replace(" ", "-"));
+    addToIndex(lowerName.replace(" ", "_"));
   } else {
     addToIndex(lowerName);
   }
 
   // Add all of the aliases to the index.
-  if (language.hasOwnProperty('aliases')) {
+  if (language.hasOwnProperty("aliases")) {
     language.aliases.forEach(addToIndex);
   }
 
@@ -206,7 +205,7 @@ for (let [name, language] of Object.entries(languages)) {
   // there's no reconciliation. So if the key is already taken, just skip.
   // Need to think of a better way to do this, since more popular languages
   // could be overridden by less popular ones.
-  if (language.hasOwnProperty('extensions')) {
+  if (language.hasOwnProperty("extensions")) {
     language.extensions.forEach(addToExtensions);
   }
 }
@@ -219,7 +218,7 @@ for (const [key, value] of extensionIndex) {
   }
 }
 
-console.log('\nSaving results...');
+console.log("\nSaving results...");
 
 // Convert the index map to a json object with ordered keys so that it looks
 // nice. Maps unfortunately don't JSON.stringify very well.
@@ -228,10 +227,10 @@ const indexObj = [...index.keys()].sort().reduce((obj, key) => {
   return obj;
 }, {});
 
-fs.writeFileSync('./lib/aliases.json', JSON.stringify(indexObj, null, 2));
+fs.writeFileSync("./lib/aliases.json", JSON.stringify(indexObj, null, 2));
 
 function slugify(s) {
-  return s.toLowerCase().replace(' ', '-');
+  return s.toLowerCase().replace(" ", "-");
 }
 
-console.log('Done!');
+console.log("Done!");
